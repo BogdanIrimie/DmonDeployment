39dbc662e736ffc634aa62ebfffdf1ecd45dd2d2
Irimie Bogdan <irimie.bogdan@outlook.com>
2016-03-01 14:45:00 +0200

    Change paths for MOS os.

--- cookbooks/dmon/attributes/default.rb
+++ cookbooks/dmon/attributes/default.rb
@@ -1,28 +1,28 @@
 
 # FrontEnd
 default['dmon']['frontend']['remote_location'] = 'https://bitbucket.org/specs-team/specs-monitoring-nmap/downloads/specs_monitoring_nmap_frontend.tar.gz' 
-default['dmon']['frontend']['archive_path'] = '/home/ubuntu/specs_monitoring_nmap_frontend.tar.gz'
-default['dmon']['frontend']['deployment_directory'] = '/home/ubuntu'
-default['dmon']['frontend']['etc_directory'] = '/home/ubuntu/specs_monitoring_nmap_frontend/etc'
-default['dmon']['frontend']['conf_file'] = '/home/ubuntu/specs_monitoring_nmap_frontend/etc/conf.properties'
+default['dmon']['frontend']['archive_path'] = '/root/specs_monitoring_nmap_frontend.tar.gz'
+default['dmon']['frontend']['deployment_directory'] = '/root'
+default['dmon']['frontend']['etc_directory'] = '/root/specs_monitoring_nmap_frontend/etc'
+default['dmon']['frontend']['conf_file'] = '/root/specs_monitoring_nmap_frontend/etc/conf.properties'
 
 # Scanner
 default['dmon']['scanner']['remote_location'] = 'https://bitbucket.org/specs-team/specs-monitoring-nmap/downloads/specs_monitoring_nmap_scanner.tar.gz'
-default['dmon']['scanner']['archive_path'] = '/home/ubuntu/specs_monitoring_nmap_scanner.tar.gz'
-default['dmon']['scanner']['deployment_directory'] = '/home/ubuntu'
-default['dmon']['scanner']['etc_directory'] = '/home/ubuntu/specs_monitoring_nmap_scanner/etc'
-default['dmon']['scanner']['conf_file'] = '/home/ubuntu/specs_monitoring_nmap_scanner/etc/conf.properties'
+default['dmon']['scanner']['archive_path'] = '/root/specs_monitoring_nmap_scanner.tar.gz'
+default['dmon']['scanner']['deployment_directory'] = '/root'
+default['dmon']['scanner']['etc_directory'] = '/root/specs_monitoring_nmap_scanner/etc'
+default['dmon']['scanner']['conf_file'] = '/root/specs_monitoring_nmap_scanner/etc/conf.properties'
 
 # Converter
 default['dmon']['converter']['remote_location'] = 'https://bitbucket.org/specs-team/specs-monitoring-nmap/downloads/specs_monitoring_nmap_converter.tar.gz'
-default['dmon']['converter']['archive_path'] = '/home/ubuntu/specs_monitoring_nmap_converter.tar.gz'
-default['dmon']['converter']['deployment_directory'] = '/home/ubuntu'
-default['dmon']['converter']['etc_directory'] = '/home/ubuntu/specs_monitoring_nmap_converter/etc'
-default['dmon']['converter']['conf_file'] = '/home/ubuntu/specs_monitoring_nmap_converter/etc/conf.properties'
+default['dmon']['converter']['archive_path'] = '/root/specs_monitoring_nmap_converter.tar.gz'
+default['dmon']['converter']['deployment_directory'] = '/root'
+default['dmon']['converter']['etc_directory'] = '/root/specs_monitoring_nmap_converter/etc'
+default['dmon']['converter']['conf_file'] = '/root/specs_monitoring_nmap_converter/etc/conf.properties'
 
 # Presenter
 default['dmon']['presenter']['remote_location'] = 'https://bitbucket.org/specs-team/specs-monitoring-nmap/downloads/specs_monitoring_nmap_presenter.tar.gz'
-default['dmon']['presenter']['archive_path'] = '/home/ubuntu/specs_monitoring_nmap_presenter.tar.gz'
-default['dmon']['presenter']['deployment_directory'] = '/home/ubuntu'
-default['dmon']['presenter']['etc_directory'] = '/home/ubuntu/specs_monitoring_nmap_presenter/etc'
-default['dmon']['presenter']['conf_file'] = '/home/ubuntu/specs_monitoring_nmap_presenter/etc/conf.properties'
+default['dmon']['presenter']['archive_path'] = '/root/specs_monitoring_nmap_presenter.tar.gz'
+default['dmon']['presenter']['deployment_directory'] = '/root'
+default['dmon']['presenter']['etc_directory'] = '/root/specs_monitoring_nmap_presenter/etc'
+default['dmon']['presenter']['conf_file'] = '/root/specs_monitoring_nmap_presenter/etc/conf.properties'
