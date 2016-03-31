a86b68e6ca3757f3071aa4a84a05f5826cc39c84
Irimie Bogdan <irimie.bogdan@outlook.com>
2016-03-31 22:08:26 +0300

    Only create RabbitMQ user if it does not exist.

--- cookbooks/dmon/recipes/frontend.rb
+++ cookbooks/dmon/recipes/frontend.rb
@@ -4,12 +4,12 @@
 #
 # Copyright (c) 2016 Bogdan-Constantin Irimie, All Rights Reserved.
 
-# Install the required packages
+# Install the required packages.
 package "mos-oracle-java-jdk-8"
 package "mos-mongodb-org"
 package "mos-rabbitmq-server"
 
-#Start RabbitMQ server
+#Start RabbitMQ server.
 service "mos-rabbitmq-server_start" do
     not_if do ::File.exists?("/var/run/rabbitmq/pid") end
     provider Chef::Provider::Service::Systemd
@@ -17,14 +17,19 @@ service "mos-rabbitmq-server_start" do
     action :start
 end
 
-
-#Create RabbitMQ user.
+#Create RabbitMQ user if it does not exist.
 bash 'create_rabbitmq_user' do
     user "root"
     code <<-EOH
-        rabbitmqctl add_user bogdan constantin
-        rabbitmqctl set_user_tags bogdan administrator
-        rabbitmqctl set_permissions -p / bogdan ".*" ".*" ".*"
+        if [[ $(rabbitmqctl list_users) == *"bogdan"* ]]
+        then
+          echo "User bogdan exists, no further action is required."
+        else
+          rabbitmqctl add_user bogdan constantin
+          rabbitmqctl set_user_tags bogdan administrator
+          rabbitmqctl set_permissions -p / bogdan ".*" ".*" ".*"
+          echo "New user bogdan was created."
+        fi
     EOH
 end
 
